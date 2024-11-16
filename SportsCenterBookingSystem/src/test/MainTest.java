e5b710b6c8ef38edeb121665833217ee05deac31
wzhang682 <160386127+wzhang682@users.noreply.github.com>
2024-11-16 16:49:01 +0800

    Add files via upload

--- SportsCenterBookingSystem/src/test/MainTest.java
+++ SportsCenterBookingSystem/src/test/MainTest.java
@@ -1,5 +1,47 @@
 package test;
 
-public class MainTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 
-}
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import execute.Admin;
+import execute.Booking;
+import execute.CmdAddNewRoom;
+import execute.Main;
+import execute.NormalUser;
+import execute.Room;
+import execute.RoomType;
+import execute.SportsCenter;
+import execute.User;
+import execute.UserRole;
+import execute.UserSessionManager;
+
+public class MainTest {
+	
+	
+//	@Test
+//	public void testRegist() {
+//	    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+//	    PrintStream originalOut = System.out;
+//	    Scanner testScanner = new Scanner("r\nA\n003\n123456\n123456\nl\n"); 
+//	    Main.scanner = testScanner; 
+//
+//	    Main.main(new String[]{});
+//	    Main.scanner.close();
+//	}
+//	
+
+	
