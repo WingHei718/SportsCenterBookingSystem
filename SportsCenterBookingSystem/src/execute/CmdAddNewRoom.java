de6aaa59d00923c93f67f428eb4bc1e55a5db2f0
WONG Ka In Karin <kikwong5-c@my.cityu.edu.hk>
2024-11-30 23:18:15 +0800

    bug007

--- SportsCenterBookingSystem/src/execute/CmdAddNewRoom.java
+++ SportsCenterBookingSystem/src/execute/CmdAddNewRoom.java
@@ -12,14 +12,11 @@ public class CmdAddNewRoom implements Command{
     	
     	int roomTypeCount = sportsCenter.printAllRoomType();
     	
-    	if (roomTypeCount==0) {
-    		action = "t";
-    		
-    	} else {
+    	
     		System.out.println("Please input your action ([t] for add room type, [r] for add room to existing room type):");
 			String[] validCommand= {"t","r"};
 			action = Common.getValidInput(scanner, validCommand, Common.InputType.COMMAND);
-    	}
+    	
     	
 		switch(action) {
             case "t":
